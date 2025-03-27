b1fc4644d346ff58349b13ab01fdd716afb15003
CMThomas-beep <candacewhitsett@gmail.com>
2025-03-26 20:13:36 -0700

    fixed bug

--- src/SettingsForm.cs
+++ src/SettingsForm.cs
@@ -1,103 +1,101 @@
+﻿/* Originally based on project by Frank McCown in 2010 */
+
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
-using System.Text;
+using System.Reflection;
 using System.Windows.Forms;
-using System.Globalization;
+using ScreenSaver.Properties;
 
-namespace FlipIt
+namespace ScreenSaver
 {
     public partial class SettingsForm : Form
     {
-        private FlipItSettings settings;
-        private List<TimeZoneInfo> timeZones;
+        private readonly FlipItSettings _settings;
+        private readonly List<Location> _availableCities = new List<Location>();
+        private readonly AutoCompleteStringCollection _cityAutoCompleteSource = new AutoCompleteStringCollection();
 
-        public SettingsForm(FlipItSettings currentSettings)
+        public SettingsForm(FlipItSettings settings)
         {
             InitializeComponent();
-            settings = new FlipItSettings
-            {
-                Use24HourFormat = currentSettings.Use24HourFormat,
-                TimeZoneId = currentSettings.TimeZoneId,
-                ShowCityName = currentSettings.ShowCityName
-            };
-            
-            LoadTimeZones();
-            PopulateControls();
+            _settings = settings;
+            versionLabel.Text = $"Version {GetVersion()}";
         }
 
-        private void LoadTimeZones()
+        private string GetVersion()
         {
-            try
-            {
-                timeZones = TimeZoneInfo.GetSystemTimeZones().OrderBy(tz => tz.DisplayName).ToList();
-            }
-            catch (Exception)
-            {
-                timeZones = new List<TimeZoneInfo>();
-            }
+            var version = typeof(SettingsForm).Assembly.GetName().Version;
+            return $"{version.Major}.{version.Minor}.{version.Build}";
         }
 
-        private void PopulateControls()
+        private void SaveSettings()
         {
-            use24HourFormatCheckBox.Checked = settings.Use24HourFormat;
-            showCityNameCheckBox.Checked = settings.ShowCityName;
-            
-            timeZoneComboBox.Items.Clear();
-            timeZoneComboBox.Items.Add("(Local Time)");
-            
-            foreach (var tz in timeZones)
-            {
-                timeZoneComboBox.Items.Add(tz.DisplayName);
-            }
-            
-            // Select current time zone
-            if (string.IsNullOrEmpty(settings.TimeZoneId))
-            {
-                timeZoneComboBox.SelectedIndex = 0;
-            }
-            else
-            {
-                var index = timeZones.FindIndex(tz => tz.Id == settings.TimeZoneId);
-                timeZoneComboBox.SelectedIndex = index >= 0 ? index + 1 : 0;
-            }
-        }
-
-        public FlipItSettings GetSettings()
-        {
-            return settings;
+            _settings.Scale = scaleTrackBar.Value * 10;
+            _settings.Save();
         }
 
         private void okButton_Click(object sender, EventArgs e)
         {
-            settings.Use24HourFormat = use24HourFormatCheckBox.Checked;
-            settings.ShowCityName = showCityNameCheckBox.Checked;
-            
-            // Get selected time zone
-            if (timeZoneComboBox.SelectedIndex == 0)
-            {
-                settings.TimeZoneId = string.Empty;
-            }
-            else if (timeZoneComboBox.SelectedIndex > 0)
-            {
-                int index = timeZoneComboBox.SelectedIndex - 1;
-                if (index >= 0 && index < timeZones.Count)
-                {
-                    settings.TimeZoneId = timeZones[index].Id;
-                }
-            }
-            
-            DialogResult = DialogResult.OK;
+            SaveSettings();
             Close();
         }
 
         private void cancelButton_Click(object sender, EventArgs e)
         {
-            DialogResult = DialogResult.Cancel;
             Close();
         }
-    }
-}
+
+        private void SettingsForm_Load(object sender, EventArgs e)
+        {
+            display24hrRadioButton.Checked = _settings.Display24HrTime;
+            display12hrRadioButton.Checked = !_settings.Display24HrTime;
+            showDstIndicatorCheckBox.Checked = _settings.ShowDstIndicator;
+            scaleTrackBar.Value = _settings.Scale / 10;
+
+            screensListBox.Items.Clear();
+            foreach (var screen in _settings.ScreenSettings)
+            {
+                screensListBox.Items.Add(screen);
+            }
+            screensListBox.SelectedIndex = 0;
+        }
+
+        private void DisplayScreenDetails()
+        {
+            var screenSettings = GetCurrentScreenSettings();
+            selectedScreenNameLabel.Text = screenSettings.Description;
+
+            switch (screenSettings.DisplayType)
+            {
+                case DisplayType.None:
+                    displayNothingRadioButton.Checked = true;
+                    break;
+                case DisplayType.CurrentTime:
+                    displayCurrentTimeRadioButton.Checked = true;
+                    break;
+                case DisplayType.WorldTime:
+                    displayWorldTimesRadioButton.Checked = true;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            worldTimesListView.Items.Clear();
+            foreach (var location in screenSettings.Locations)
+            {
+                AddLocationToListView(location);
+            }
+            CheckWorldTimesControls();
+        }
+
+        private ScreenSetting GetCurrentScreenSettings()
+        {
+            return (ScreenSetting)screensListBox.SelectedItem;
+        }
+
+        private void CheckWorldTimesControls()
+        {
+            var allowLocationEditing = displayWorldTimesRadioButton.Checked;
+            worldTimesListView.Enabled
