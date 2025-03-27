ff3c32dc76213d63d69280051269f189b7a44c5d
CMThomas-beep <candacewhitsett@gmail.com>
2025-03-26 18:49:34 -0700

    add the rest

---
+++ src/CurrentTimeScreen.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace ScreenSaver
+{
+    internal class CurrentTimeScreen : TimeScreen
+    {
+        private readonly bool _display24HourTime;
+        private readonly bool _isPreviewMode;
+        private readonly bool _showSeconds = false;
+
+        private const int SplitWidth = 4;
+        private const double BoxSeparationPercent = 0.05; // ie. 5%
+
+        private Font _largeFont;
+        private Font _smallFont;
+
+        private Font LargeFont => _largeFont ?? (_largeFont = new Font(FontFamily, _boxSize.Percent(85), FontStyle.Bold, GraphicsUnit.Pixel));
+        private Font SmallFont => _smallFont ?? (_smallFont = new Font(FontFamily, _boxSize.Percent(9), FontStyle.Bold, GraphicsUnit.Pixel));
+
+        private readonly Brush _fontBrush = new SolidBrush(Color.FromArgb(255, 183, 183, 183));
+        private readonly Pen _splitPen = new Pen(Color.Black, SplitWidth);
+
+        private readonly int _boxSize;
+        private readonly int _separatorWidth;
+        private readonly int _startingX;
+        private readonly int _startingY;
+
+        private const bool DrawGuideLines = false;
+
+
+        public CurrentTimeScreen(Form form, bool display24HourTime, bool isPreviewMode, int scalePercent)
+        {
+            _display24HourTime = display24HourTime;
+            _isPreviewMode = isPreviewMode;
+            _form = form;
+            
+            // The border is between 5% and 30% of the screen
+            //  * A scale of 0 = 5% 
+            //  * A scale of 100 = 30%
+            var borderPercent = (100 - scalePercent) / 4 + 5;
+            
+            var boxSizeWidth = CalcBoxSize(form.Width, borderPercent, 2);
+            var boxSizeHeight = CalcBoxSize(form.Height, borderPercent, 1);
+            
+            _boxSize = Math.Min(boxSizeWidth, boxSizeHeight);
+            _separatorWidth = Convert.ToInt32(_boxSize * BoxSeparationPercent);
+
+            _startingX = CalcOffset(form.Width, 2, _boxSize, _separatorWidth);
+            _startingY = CalcOffset(form.Height, 1, _boxSize, 0);
+        }
+
+        private int CalcBoxSize(int total, int borderPercent, int boxCount)
+        {
+            var borderSize = total.Percent(borderPercent);
+            var remainingSpace = total - (borderSize * 2);
+            var parts = (1 + BoxSeparationPercent) * boxCount - BoxSeparationPercent;
+            return Convert.ToInt32(remainingSpace / parts);
+        }
+
+        private int CalcOffset(int total, int boxCount, int boxSize, int seperatorSize )
+        {
+            var sizeOfAllBoxes = (boxSize + seperatorSize) * boxCount - seperatorSize;
+            return (total - sizeOfAllBoxes) / 2;
+        }
+        
+        protected override byte[] GetFontResource()
+        {
+            return Properties.Resources.HelveticaLTStd_BoldCond;
+        }
+        
+        internal override void Draw()
+        {
+            var boxRect = new Rectangle(_startingX, _startingY, _boxSize, _boxSize);
+
+            if (!_display24HourTime)
+            {
+                var pm = SystemTime.Now.Hour >= 12;
+                DrawIt(boxRect, SystemTime.Now.ToString("%h"), pm ? null : "AM", pm ? "PM" : null); // The % avoids a FormatException https://msdn.microsoft.com/en-us/library/8kb3ddd4(v=vs.110).aspx#UsingSingleSpecifiers
+            }
+            else
+            {
+                DrawIt(boxRect, SystemTime.Now.ToString("HH"));
+            }
+
+            boxRect.X += _boxSize + _separatorWidth;
+            DrawIt(boxRect, SystemTime.Now.ToString("mm"));
+
+            if (_showSeconds)
+            {
+                boxRect.X += _boxSize + _separatorWidth;
+                DrawIt(boxRect, SystemTime.Now.ToString("ss"));
+            }
+        }
+
+        private void DrawIt(Rectangle rect, string s, string topString = null, string bottomString = null)
+        {
+            DrawBox(rect);
+            DrawTextInRect(rect, s, topString, bottomString);
+        }
+        
+        private void DrawBox(Rectangle rect)
+        {
+            var radius = rect.Width / 20;
+            using (var path = RoundedRectangle.Create(rect, radius))
+            using (var brush = new LinearGradientBrush(rect,
+                BackColorTop,
+                BackColorBottom, LinearGradientMode.Vertical))
+            {
+                Gfx.FillPath(brush, path);
+            }
+        }
+        
+        private void DrawTextInRect(Rectangle rect, string s, string topString = null, string bottomString = null)
+        {
+            var diff = rect.Width / 10;
+            
+            // Some hacky adjustments to center the text in the box
+            var xOffset = rect.Width.Percent(1);
+            var yOffset = rect.Height.Percent(4);
+            
+            var textRect = new Rectangle(rect.Left - diff + xOffset, rect.Y + yOffset, rect.Width + diff * 2, rect.Height);
+
+            if (DrawGuideLines)
+            {
+                Gfx.DrawRectangle(Pens.Red, textRect);
+            }
+            
+            // Draw the text
+            var stringFormat = new StringFormat
+            {
+                Alignment = StringAlignment.Center, 
+                LineAlignment = StringAlignment.Center,
+                //FormatFlags = StringFormatFlags.NoWrap
+            };
+
+            Gfx.DrawString(s, LargeFont, _fontBrush, textRect, stringFormat);
+
+            if (topString != null)
+            {
+                var leftOffset = diff / 2;
+                Gfx.DrawString(topString, SmallFont, _fontBrush, rect.X + leftOffset, rect.Y + diff);
+            }
+            if (bottomString != null)
+            {
+                var leftOffset = diff / 2;
+                Gfx.DrawString(bottomString, SmallFont, _fontBrush, rect.X + leftOffset, rect.Bottom - diff - SmallFont.Height);
+            }
+
+            // Horizontal dividing line
+            if (!_isPreviewMode)
+            {
+                var y = rect.Y + (rect.Height / 2) - (SplitWidth / 2);
+                Gfx.DrawLine(_splitPen, rect.Left, y, rect.Right, y);
+            }
+            else
+            {
+                var y = rect.Y + (rect.Height / 2);
+                Gfx.DrawLine(Pens.Black, rect.Left, y, rect.Right, y);
+            }
+        }
+    }
+}
---
+++ src/TimeScreen.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Text;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace ScreenSaver
+{
+    internal abstract class TimeScreen
+    {
+        protected Form _form;
+        private Graphics _graphics;
+        private PrivateFontCollection _pfc = null;
+        private FontFamily _fontFamily = null;
+
+        protected abstract byte[] GetFontResource();
+
+        internal abstract void Draw();
+
+        protected Graphics Gfx
+        {
+            get
+            {
+                if (_graphics == null)
+                {
+                    _graphics = _form.CreateGraphics();
+                    _graphics.TextRenderingHint = TextRenderingHint.AntiAlias;
+                    _graphics.SmoothingMode = SmoothingMode.HighQuality;
+                }
+                return _graphics ;
+            }
+        }
+
+        protected FontFamily FontFamily
+        {
+            get
+            {
+                if (_fontFamily == null)
+                {
+                    if (_pfc == null)
+                    {
+                        _pfc = InitFontCollection();
+                    }
+                    _fontFamily = _pfc.Families[0];
+                }
+                return _fontFamily ?? (_fontFamily = _pfc.Families[0]);
+            }
+        }
+
+        // Experimental
+        protected int GetFontAscentPercent()
+        {
+            var ascent = FontFamily.GetCellAscent(FontStyle.Regular);
+            var all =  FontFamily.GetEmHeight(FontStyle.Regular);
+            return ascent * 100 / all;
+        }
+        
+        private PrivateFontCollection InitFontCollection()
+        {
+            // We don't add both fonts at the same time because I can only get the private font collection
+            // to return the first one we add. If the first one is the non-bold one and we ask for a bold one
+            // then it seems to have an (inadequate) go at generating bold rather than using the one we gave it.
+            // The system font collection does not seem to have this problem.
+            // protected abstract PrivateFontCollection InitFontCollection();
+
+            var pfc = new PrivateFontCollection();
+            AddFont(pfc, GetFontResource());
+            return pfc;
+        }
+        
+        protected static readonly Color BackColorTop = Color.FromArgb(255, 18, 18, 18);
+        protected static readonly Color BackColorBottom = Color.FromArgb(255, 10, 10, 10);
+        protected static readonly Brush FontBrush = new SolidBrush(Color.FromArgb(255, 183, 183, 183));
+        
+        protected static void AddFont(PrivateFontCollection pfc, byte[] fontResource)
+        {
+            IntPtr ptr = Marshal.AllocCoTaskMem(fontResource.Length);  // create an unsafe memory block for the font data
+            Marshal.Copy(fontResource, 0, ptr, fontResource.Length);  // copy the bytes to the unsafe memory block
+            pfc.AddMemoryFont(ptr, fontResource.Length);    // pass the font to the font collection
+            Marshal.FreeCoTaskMem(ptr);
+        }
+
+        protected string FormatAmPm(DateTime time)
+        {
+            // We format this ourselves because some cultures, such as nl-NL produce, results longer than 2 chars. ie. "a.m."
+            // and we want to be consistent with the current time screen
+            return time.Hour >= 12 ? "PM" : "AM";
+        }
+
+        internal void DisposeResources()
+        {
+            if (_fontFamily != null)
+            {
+                _fontFamily.Dispose();
+                _fontFamily = null;
+            }
+            if (_pfc != null)
+            {
+                _pfc.Dispose();
+                _pfc = null;
+            }
+        }
+    }
+}
